af201709e825eb82ba8e9e81d103566e4e000dd4
cjy <cjy632258@hotmail.com>
2017-12-12 15:36:50 +0800

    cli

---
+++ cli/run.sh
@@ -0,0 +1,16 @@
+if [ -f "${SUPERVISOR_CONF}" ]; then
+    cp ${SUPERVISOR_CONF} /etc/supervisor/default.conf
+fi
+if [ -f "${CRONTAB_FILE}" ]; then
+    cp ${CRONTAB_FILE} /etc/crontabs/root
+fi
+
+if [ "${TIMEZONE}" == "" ]; then
+   TIMEZONE='Asia/Shanghai'
+fi
+
+echo ${TIMEZONE} > /etc/timezone && \
+ln -sf /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
+
+supervisord --configuration /etc/supervisor/supervisord.conf
+/usr/sbin/crond
