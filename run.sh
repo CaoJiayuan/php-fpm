113a294b939c50f185e1382489abd9722e471905
cjy <cjy632258@hotmail.com>
2018-10-30 13:41:23 +0800

    -_-!!!

--- run.sh
+++ run.sh
@@ -3,9 +3,6 @@ if [ -f "${CRONTAB_FILE}" ]; then
     cp ${CRONTAB_FILE} /etc/crontabs/root
 fi
 
-echo 'clear conf'
-rm -fr /usr/local/etc/php-fpm.d/*
-
 echo "date.timezone=${TIMEZONE}" >> /usr/local/etc/php/conf.d/default.ini && \
 echo ${TIMEZONE} > /etc/timezone && \
 ln -sf /usr/share/zoneinfo/${TIMEZONE} /etc/localtime && \
