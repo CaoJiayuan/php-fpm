0adb57c6e4445bd0d0e318427e4b4dd61699e49a
caojiayuan <cjy632258@hotmail.com>
2020-05-18 14:19:13 +0800

    update

--- supervisor/supervisor-run.sh
+++ supervisor/supervisor-run.sh
@@ -3,5 +3,6 @@ if [ -f "${SUPERVISOR_CONF}" ]; then
     cp ${SUPERVISOR_CONF} /etc/supervisor/default.conf
 fi
 
+envsubst '$SUPERVISOR_CONFIG_DIR' < /etc/supervisor/supervisord.conf.template > /etc/supervisor/supervisord.conf
 supervisord --configuration /etc/supervisor/supervisord.conf
 php-fpm
